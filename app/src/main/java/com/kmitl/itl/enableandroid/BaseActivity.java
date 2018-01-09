27d4568a9ad1e8c0e5884bbf8be19a1bbfaed3b0
last3oy <softwarez_gelion@hotmail.com>
2018-01-09 23:50:14 +0700

    create MapActivity

---
+++ app/src/main/java/com/kmitl/itl/enableandroid/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.kmitl.itl.enableandroid;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity<T extends ViewDataBinding> extends AppCompatActivity {
+
+    protected T mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int layoutId = getLayoutId();
+        mBinding = DataBindingUtil.setContentView(this, layoutId);
+        initInstances();
+    }
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initInstances();
+}
